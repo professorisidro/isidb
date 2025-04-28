20c802709d47df459a38238b06f3fecc8ef58258
professorisidro <isidro@professorisidro.com.br>
2025-04-28 17:16:16 -0300

    first commit

---
+++ src/main/java/br/com/isilab/isidb/IsidbApplication.java
@@ -0,0 +1,13 @@
+package br.com.isilab.isidb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsidbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsidbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/isilab/isidb/IsidbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.isilab.isidb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsidbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
